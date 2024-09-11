0225e19604dd5b4f2e88542a5e65e1715203a31a
intilavanya <142288117+intilavanya@users.noreply.github.com>
2024-09-11 22:54:31 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\user\\Downloads\\flutter\\.git\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\user\\OneDrive\\Desktop\\LAVANYAGPT\\LAVANYAGPT" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\user\\Downloads\\flutter\\.git\\flutter"
+  "PROJECT_DIR=C:\\Users\\user\\OneDrive\\Desktop\\LAVANYAGPT\\LAVANYAGPT"
+  "FLUTTER_ROOT=C:\\Users\\user\\Downloads\\flutter\\.git\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\user\\OneDrive\\Desktop\\LAVANYAGPT\\LAVANYAGPT\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\user\\OneDrive\\Desktop\\LAVANYAGPT\\LAVANYAGPT"
+  "FLUTTER_TARGET=C:\\Users\\user\\OneDrive\\Desktop\\LAVANYAGPT\\LAVANYAGPT\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC8yMzVkYjkxMWJhMjc5NzIyZjVlNjg1ZjM4YjBlZDMwZmE3ZTg1NzBhLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\user\\OneDrive\\Desktop\\LAVANYAGPT\\LAVANYAGPT\\.dart_tool\\package_config.json"
+)
